c3757fc9f6998d8fb1f5b813e58418a3ff583bb5
Aniket Bhalerao <109614388+Aniket01996@users.noreply.github.com>
2023-09-07 18:55:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("btn");
+button.addEventListener('click', function () {
+    button.style.text = "OFF";
+    button.style.backgroundColor = "green";
+    console.log("Clicked");
+})
